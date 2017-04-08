e784c9476b48d96f23ca0fe1869eab0a6bcaeed2
xun <xun@debian-xun>
2017-04-08 14:49:46 +0800

    Unit03 first

---
+++ Unit03/age.py
@@ -0,0 +1,12 @@
+try:
+	age = int(input('請輸入年齡？'))
+	if age >= 20:
+		print('你已有投票權!!')
+	else:
+		print('你目前要等到20歲以上才有投票權，乖喔～。')
+except Exception as e:
+	pirnt('請輸入數字…謝謝')
+else:
+	pass
+finally:
+	pass
