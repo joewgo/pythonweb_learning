11ded7bf5c349c36baf147031d400612ef189b66
xun <xun@debian-xun>
2017-04-17 23:19:06 +0800

    Unit06_0

---
+++ Unit06/ModuleHero/index.py
@@ -0,0 +1,12 @@
+#import hello
+#hello.print_helloworld()
+#from hello import print_helloworld
+#print_helloworld()
+
+#from hello import print_helloworld as phw
+#phw()
+from mypackage.awesome import print_awesome
+from mypackage.happy import print_happy
+
+print_awesome()
+print_happy()
